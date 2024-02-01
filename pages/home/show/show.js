f1b1b457c0ed30f70648cfa62120117e4cde5db8
Niggling <272744476@qq.com>
2024-02-01 13:57:10 +0800

    修改：优化展示页面效果

--- pages/home/show/show.js
+++ pages/home/show/show.js
@@ -1,13 +1,13 @@
 // pages/home/show/show.js
 Page({
   data: {
-    currentTab: 'introduction', // 默认显示介绍标签
+    currentTab: 'introduction',
     artworkInfo: {
       createTime: '2023年9月3日',
       type: '藏画',
       size: '100厘米×70厘米',
     },
-    // 其他数据...
+    scale: 1,
   },
   
   onLoad(options) {
@@ -27,7 +27,13 @@ Page({
 
   playVoiceIntroduction() {
     // 处理语音介绍的逻辑...
-  }
+  },
 
-
+  onScroll(e) {
+    const { scrollTop } = e.detail;
+    const scale = 1 - 0.005 * scrollTop;
+    this.setData({
+      scale: scale < 0.7 ? 0.7 : scale,
+    });
+  },
 });
